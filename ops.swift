4ff53c09daf28f94e91b12d91cf8224db3d3e583
Ezra Hampton <ezra.hamptoniii@gmail.com>
2022-01-29 21:02:14 -0500

    turning in for the night

---
+++ ops.swift
@@ -0,0 +1,15 @@
+//Arithmetic Operators
+
+/*
+    + addition
+    - subtraction
+    * multiplication
+    / division
+    % modulo (divides and gives the remainder)
+*/
+
+var amount = 20.00
+// Write your code below 🧾
+
+var tip = amount * 0.2 //should be 4.0
+print(tip)
