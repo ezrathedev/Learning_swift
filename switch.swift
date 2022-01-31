56b26b32e402743f3a5b6a2d61295c15bef91c77
Ezra Hampton <ezra.hamptoniii@gmail.com>
2022-01-30 22:14:08 -0500

    add switch to the bunch

---
+++ switch.swift
@@ -0,0 +1,13 @@
+switch secretIdentity {
+  case "Tony Stark":
+  superheroName = "Iron Man"
+  case "Natasha Romanoff":
+  superheroName = "Black Widow"
+  case "Prince T'Challa":
+  superheroName = "Black Panther"
+  case "Thor":
+  superheroName = "Thor"
+  default:
+    superheroName = "Unknown"
+}
+print(superheroName)
